c868615bdfe2987b02bf1bcd687bc395e4a415d8
김준환 <musoyou1085@gmail.com>
2025-11-03 11:01:06 +0900

    chore: entity setting

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "../users/user.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Report {
@@ -26,4 +27,7 @@ export class Report {
 
     @Column()
     price: number;
+
+    @ManyToOne(() => User, (user) => user.reports)
+    user: User;
 }
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,5 +1,5 @@
-import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from "typeorm";
-import { Exclude } from "class-transformer";
+import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+import { Report } from "../reports/report.entity";
 
 @Entity()
 export class User {
@@ -12,6 +12,9 @@ export class User {
 
     @Column()
     password: string;
+
+    @OneToMany(() => Report, (report) => report.user)
+    reports: Report[];
 
     @AfterInsert()
     logInsert() {
