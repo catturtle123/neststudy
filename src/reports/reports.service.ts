bf5f4c1bf3e98b36aa02eeec71fd4537b3e1b41b
김준환 <musoyou1085@gmail.com>
2025-11-07 15:48:07 +0900

    feat: getEstimate 기능 추가

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,11 +4,27 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Report } from './report.entity';
 import { User } from '../users/user.entity';
+import { GetEstimateDto } from './dto/get-estimate-dto';
 
 @Injectable()
 export class ReportsService {
     constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
+    createEstimate(query: GetEstimateDto) {
+        return this.repo.createQueryBuilder()
+            .select('AVG(prive)', 'price')
+            .where('make = :make', { make: query.make })
+            .andWhere('model = :model', { model: query.model })
+            .andWhere('lng - :lng BETWEEN -5 AND 5', { lng: query.lng })
+            .andWhere('lat - :lat BETWEEN -5 AND 5', { lat: query.lat })
+            .andWhere('year - :year BETWEEN -3 AND 3', { year: query.year })
+            .andWhere('apporved IS TRUE')
+            .orderBy('ABS(mileage - :mileage)', 'DESC')
+            .setParameters({ mileage: query.mileage})
+            .limit(3)
+            .getRawOne();
+    }
+    
     create(user: User, reportDto: CreateReportDto) {
         const report = this.repo.create(reportDto);
         report.user = user;
